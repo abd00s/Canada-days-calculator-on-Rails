a1976bba015d98258ee12e87f4eb40b181396ba8
Abdullah <abd00s@users.noreply.github.com>
2014-11-05 18:40:52 -0500

    can modify

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,24 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])		
 	end
 
+	def edit
+		@event = Event.find(params[:id])	
+	end
+
+	def update
+		@event = Event.find(params[:id])
+
+		if @event.update_attributes(event_params)
+			redirect_to event_path(@event)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		
+	end
+
 	private
 	def event_params
 		params.require(:event).permit(:date_from, :date_to, :location, :description)
