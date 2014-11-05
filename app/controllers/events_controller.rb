6fa26a1971a0277032ecaa8e78acdb1b27964b76
Abdullah <abd00s@users.noreply.github.com>
2014-11-05 17:56:46 -0500

    can add new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,4 +9,19 @@ class EventsController < ApplicationController
 	def new
 		@event = Event.new
 	end
+
+	def create
+		# binding.pry
+		@event = Event.new(event_params)
+		if @event.save
+			redirect_to events_path
+		else
+			render :new
+		end
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:date_from, :date_to, :location, :description)
+	end
 end
